aac4a3690f2c17e36e4a8c1e11fc5095896cd25d
Cool Lime <mz.zhoumeng@gmail.com>
2019-03-15 01:11:37 -0700

    TaskEntry Defined
    Defined the database table "task" using the TaskEntry class

---
+++ app/src/main/java/com/coollime/archtasks/database/TaskEntry.java
@@ -0,0 +1,68 @@
+package com.coollime.archtasks.database;
+
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.Ignore;
+import android.arch.persistence.room.PrimaryKey;
+
+import java.util.Date;
+
+@Entity(tableName = "task")
+public class TaskEntry {
+
+    // The id will be used a primary key and Room will generate it for us
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+    private String description;
+    private int priority;
+    // Make sure the column name is straightforward
+    @ColumnInfo(name = "updated_at")
+    private Date updatedAt;
+
+    @Ignore
+    public TaskEntry(String description, int priority, Date updatedAt) {
+        // Room will not use this constructor
+        this.description = description;
+        this.priority = priority;
+        this.updatedAt = updatedAt;
+    }
+
+    public TaskEntry(int id, String description, int priority, Date updatedAt) {
+        this.id = id;
+        this.description = description;
+        this.priority = priority;
+        this.updatedAt = updatedAt;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
